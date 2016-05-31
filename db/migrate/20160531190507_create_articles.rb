000cff86447ec0d3d6f1ba3cf61ab257bea87574
QuickResolve <lauranmontoya@gmail.com>
2016-05-31 14:34:51 -0700

    add img_url column to db

--- db/migrate/20160531190507_create_articles.rb
+++ db/migrate/20160531190507_create_articles.rb
@@ -3,6 +3,7 @@ class CreateArticles < ActiveRecord::Migration
     create_table :articles do |t|
       t.string :title
       t.text :text
+      t.string :img_url
 
       t.timestamps null: false
     end
