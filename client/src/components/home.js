ef6210e8d305d18dfeb0fa3eee2a9a0646b403fe
Laura Montoya <lauranmontoya@gmail.com>
2020-03-13 17:04:25 -0700

    add featured logos and seen component

--- client/src/components/home.js
+++ client/src/components/home.js
@@ -3,12 +3,14 @@ import React from 'react';
 
 import LandingPage from './landing_page';
 import About from './about';
+import Seen from './seen';
 
 export default props =>{
     return(
         <div className='home'>
             <LandingPage/>
             <About/>
+            <Seen/>
         </div>
     )
 }
